de2d3df725732f5c9e8157b64f956b9c5bbd31d2
cyy <55910336+viko-3@users.noreply.github.com>
2024-01-11 15:15:50 +0900

    Delete DL/tmp.py

--- DL/tmp.py
+++
@@ -1,7 +0,0 @@
-def remove_prefix(state_dict):
-    return {k.replace('module.', ''): v for k, v in state_dict.items()}
-
-# safeload some parameters from pretrained model molscribe
-molscribe_path = "/home/wangzixu/MolScribe/ckpts/swin_base_char_aux_200k.pth"
-pretrained_dict = torch.load(molscribe_path, map_location=torch.device('cpu'))["encoder"]
-model.load_state_dict(remove_prefix(pretrained_dict), strict=False)
